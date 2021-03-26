21c357a06a8a778d7f3fae498503569e78d28979
Mert <mertuygur02@gmail.com>
2021-03-26 16:38:59 +0300

    cart styling a bit

--- client/src/components/screens/CartScreen.js
+++ client/src/components/screens/CartScreen.js
@@ -26,35 +26,57 @@ const CartScreen = ({ match, location, history }) => {
         history.push('/login?redirect=shipping');
     }
     return (
-        <div>
-            <h1>Sepet</h1>
-            {cartItems.length === 0 ? <div><h1>Your cart is empty</h1> <NavLink to="/">Geri Dön</NavLink></div> : (
-                <div>
-                    {cartItems.map(item => (
-                        <li key={item.product}>
-                            <h2>{item.name}</h2>
-                            <img src={item.image} alt={item.name} />
-                            <p>{item.price}</p>
-                            <select type="select" value={item.qty} onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
-                                {
-                                    [...Array(item.countInStock).keys()].map((x) => (
-                                        <option key={x + 1} value={x + 1}>
-                                            {x + 1}
-                                        </option>
-                                    ))}
-                            </select>
-                            <button onClick={() => removeFromCartHandler(item.product)} >Kaldır</button>
-                        </li>
-                    ))}
-                    <div>
-                        <h2>Subtotal (
-                            {cartItems.reduce((acc, item) => acc + item.qty, 0)})</h2>
-                        ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
+        <div className="cart">
+            <div className="cart__title">
+                <h1>Sepet</h1>
+            </div>
 
-                        <button onClick={checkoutHandler}> Satın Al</button>
-                    </div>
+            {cartItems.length === 0 ?
+                <div className="cart__empty">
+                    <h1 className="cart__empty__text">Your cart is empty</h1>
+                    <NavLink to="/">Geri Dön</NavLink>
                 </div>
-            )}
+                :
+                (
+                    <div className="cart__items">
+                        {cartItems.map(item => (
+                            <li key={item.product} className="cart__items__item">
+
+                                <div className="cart__items__item__first">
+                                    <h2>{item.name}</h2>
+                                    <img src={item.image} alt={item.name} width="200px" />
+                                </div>
+
+                                <div className="cart__items__item__second">
+                                    <p>{item.price}TL</p>
+                                </div>
+
+                                <div className="cart__items__item__third">
+                                    <select type="select" value={item.qty} onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
+                                        {
+                                            [...Array(item.countInStock).keys()].map((x) => (
+                                                <option key={x + 1} value={x + 1}>
+                                                    {x + 1}
+                                                </option>
+                                            ))}
+                                    </select>
+                                </div>
+
+                                <div className="cart__items__item__fourth">
+                                    <button onClick={() => removeFromCartHandler(item.product)} >Kaldır</button>
+                                </div>
+                            </li>
+                        ))}
+
+                        <div className="cart__items__price">
+                            <h2>Subtotal (
+                                {cartItems.reduce((acc, item) => acc + item.qty, 0)})</h2>
+                            {cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}TL
+                            <button onClick={checkoutHandler}> Satın Al</button>
+                        </div>
+
+                    </div>
+                )}
         </div>
     )
 }
--- client/src/components/screens/ProductScreen.js
+++ client/src/components/screens/ProductScreen.js
@@ -20,23 +20,31 @@ const ProductScreen = ({ history, match }) => {
     }
 
     return (
-        <div>
-            <div>
+        <div className="singleproduct">
+            <div className="singleproduct__back">
                 <Link to="/">Geri Dön</Link>
             </div>
 
-            <div>
-                <img src={product.image} alt={product.name} />
+            <div className="singleproduct__image">
+                <img src={product.image} alt={product.name} width="350px" />
             </div>
 
-            <div>
-                <h2>{product.name}</h2>
-                <Rating value={product.rating} text={`${product.numReviews} Reviews`} />
-                <h2>2 TL / ADET</h2>
-                <h2>Category: {product.category}</h2>
-                <h2>Stok Adedi: {product.countInStock}</h2>
+            <div className="singleproduct__text">
+                <div className="singleproduct__text__name">
+                    <h2>{product.name}</h2>
+                </div>
+                <div className="singleproduct__text__rating">
+                    <Rating value={product.rating} text={`${product.numReviews} Reviews`} />
+                </div>
+                <div className="singleproduct__text__sub">
+                    <h2>2 TL / ADET</h2>
+                    <h2>Category: {product.category}</h2>
+                    <h2>Stok Adedi: {product.countInStock}</h2>
+                </div>
+
                 {product.countInStock > 0 && (
-                    <div>
+                    <div className="singleproduct__text__stock">
+                        <h3>Adet</h3>
                         <select type="select" value={qty} onChange={(e) => setQty(e.target.value)}>
                             {
                                 [...Array(product.countInStock).keys()].map((x) => (
@@ -48,9 +56,9 @@ const ProductScreen = ({ history, match }) => {
 
                     </div>
                 )}
-                <button
-                    onClick={addToCartHandler}
-                >Add to cart</button>
+                <div className="singleproduct__text__btn">
+                    <button onClick={addToCartHandler}>Add to cart</button>
+                </div>
             </div>
         </div>
     )
