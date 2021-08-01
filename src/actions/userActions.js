67cbe32e1b66e43572bea2aedb731fb10bc103a1
Mert <mertuygur02@gmail.com>
2021-08-01 21:37:53 +0300

    bg

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -65,7 +65,7 @@ export const register = (name, email, password) => async (dispatch) => {
       },
     };
     const { data } = await axios.post(
-      `${process.env.REACT_APP_API}/api/users`,
+      `${process.env.REACT_APP_API}/api/users/register`,
       { name, email, password },
       config
     );
